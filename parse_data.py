562844e519c824484945a9e20d535a4ea1251762
theo <>
2021-12-04 03:06:36 +0000

    document parse functions
    FossilOrigin-Name: 8f6d4724a07602625e6ae51a7cfb71430d60156bbd049d5f9110c09f0796aad3

--- parse_data.py
+++ parse_data.py
@@ -10,7 +10,11 @@ from records import CovidRecord, StockRecord
 
 
 def parse_stock_record(row: list[str], stock_name: str) -> StockRecord:
-    """TODO
+    """Parse the stock data from a row of entries into a StockeRecord object with stock_name.
+
+    Preconditions:
+        - stock_name != ''
+        - len(row) == 5
     """
     return StockRecord(
         stock=stock_name,
@@ -23,7 +27,7 @@ def parse_stock_record(row: list[str], stock_name: str) -> StockRecord:
 
 
 def parse_stock_data_file(filename: str, stock_name: str) -> list[StockRecord]:
-    """TODO
+    """Parse stock data from filename, which contains data from the stock_name stock.
     """
     with open(filename, mode='r') as file:
         reader = csv.reader(file)
@@ -33,7 +37,11 @@ def parse_stock_data_file(filename: str, stock_name: str) -> list[StockRecord]:
 
 
 def parse_covid_record(row: list[str]) -> CovidRecord:
-    """TODO
+    """Parse the stock data from a row of entries into a CovidRecord object.
+
+    Preconditions:
+        - len(row) == 3
+        - len(row[0]) == 3
     """
     return CovidRecord(
         country=row[0],
@@ -43,7 +51,9 @@ def parse_covid_record(row: list[str]) -> CovidRecord:
 
 
 def parse_covid_data_file(filename: str) -> list[CovidRecord]:
-    """TODO
+    """Parse covid data from filename.
+
+    It is expected that there will be data from multiple countries in this file.
     """
     with open(filename, mode='r') as file:
         reader = csv.reader(file)
