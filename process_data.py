0b8e8dd656f4f921e601f9fad04d4a36bfe9dc46
theo <>
2021-12-04 22:33:55 +0000

    style changes
    FossilOrigin-Name: 15de588519d1575cb9580a95efcd032a74f14654d8ea8cbceef2523fee11c685

--- process_data.py
+++ process_data.py
@@ -8,6 +8,9 @@ from parse_data import parse_covid_data_file, parse_stock_data_file
 
 class DataManager:
     """TODO
+
+    >>> usa_and_snp = DataManager({'data/stock-snp500.csv', 'data/covid-usa.csv'}, \
+                                  datetime.date(2021, 1, 1), datetime.date(2021, 1, 10))
     """
     _covid: dict[str, list[int]]
     _open: dict[str, list[float]]
@@ -44,7 +47,8 @@ class DataManager:
 
                 assert len(self._covid[name]) == self._duration
             elif 'stock-' in source:
-                dates, *data = parse_stock_data_file(source, start - datetime.timedelta(days=1), end)
+                dates, *data = parse_stock_data_file(source, start - datetime.timedelta(days=1),
+                                                     end)  # TODO ugly-ish
 
                 data = [differentiate_stock_data(x) for x in data]
                 dates.pop(0)  # the above implicitly chops off the first element
@@ -112,3 +116,6 @@ def fill_stock_data(dates: list[datetime.date], data: list[float], start: dateti
         filled_data[actual_index] = data[i]
 
     return filled_data
+
+# TODO is it safe to make the assumption that there are no gaps in the covid data?  Do we need a
+#  fill_covid_data to match fill_stock_data?
