a22c25705de82a3eba20fe26b3b466939673012e
theo <>
2021-12-09 20:36:43 +0000

    fix build issues (hopefully) by just installing the binary versions instead of trying to build them each time from scratch within the isolated environment
    FossilOrigin-Name: 0fe2be98c34b470f3f2121eb1aa024e0cb79ff8223cc67a8af48ab1080c84151

--- ci/10-system-packages.sh
+++ ci/10-system-packages.sh
@@ -3,4 +3,12 @@
 set -e
 
 apk add python3
+apk add python3-dev
 apk add py3-pip
+
+# Runtime dependencies
+apk add py3-pandas
+
+# Test dependencies
+apk add py3-pytest
+apk add py3-hypothesis
--- ci/11-pip-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd /repo
-
-pip3 install -r requirements.txt
-pip3 install -r requirements_dev.txt
