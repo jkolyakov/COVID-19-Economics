94718156e417fcbf743052bef3de9a12775a90d0
jacob <>
2021-12-11 05:41:28 +0000

    Put the code to get data for snp500 from yfinance into a function so it's actually usable
    FossilOrigin-Name: c5a78864a6e79a8a28a8385af65d4fbee2bd82a268ecf95b8657843d16dca3e1

--- get_data.py
+++ get_data.py
@@ -1,10 +1,18 @@
-import yfinance as yf  # TODO: Not in usable state (should not run immediately, create functions)
+"""
+File for getting the data from external sources. Currently only for the snp500 stock values
+"""
+
+import yfinance as yf
 import pandas as pd
 
-data = yf.download('^GSPC', start='2018-01-01', end='2021-11-20')
-print(data)
-data.to_csv("snp500-data.csv")
-f = pd.read_csv("snp500-data.csv")
-keep_col = ['Date', 'Open', 'High', 'Low', 'Close']
-new_f = f[keep_col]
-new_f.to_csv("snp500-data1.csv", index=False)
+
+def get_snp500_data() -> None:
+    """This function will download the historical data for the snp500 from yfinance
+    and format it in a csv file"""
+    data = yf.download('^GSPC', start='2018-01-01', end='2021-11-20')
+    print(data)
+    data.to_csv("snp500-data.csv")
+    f = pd.read_csv("snp500-data.csv")
+    keep_col = ['Date', 'Open', 'High', 'Low', 'Close']
+    new_f = f[keep_col]
+    new_f.to_csv("snp500-data1.csv", index=False)
