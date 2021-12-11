55157d262803b02977bacd63d1c2ea1bfe2418ce
jacob <>
2021-12-11 06:10:23 +0000

    Cleaned up commas, dates and column order for the tx60 data. Fixed up some typos in get_snp500_data and created a fix_tx60 function in get_data.py
    FossilOrigin-Name: 73863c957f3cf4db2fa1f8e2886be065f869a749d17d3aaf8ba0721490a32248

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 """
-File for getting the data from external sources. Currently only for the snp500 stock values
+File for getting/formatting the data for external sources. Currently for all stock data
 """
 
 import yfinance as yf
@@ -11,8 +11,20 @@ def get_snp500_data() -> None:
     and format it in a csv file"""
     data = yf.download('^GSPC', start='2018-01-01', end='2021-11-20')
     print(data)
-    data.to_csv("snp500-data.csv")
-    f = pd.read_csv("snp500-data.csv")
+    data.to_csv("data/snp500-data.csv")
+    f = pd.read_csv("data/snp500-data.csv")
     keep_col = ['Date', 'Open', 'High', 'Low', 'Close']
     new_f = f[keep_col]
-    new_f.to_csv("snp500-data1.csv", index=False)
+    new_f.to_csv("data/snp500-data.csv", index=False)
+
+
+def fix_tx60() -> None:
+    """This function will fix the formatting of the tx60 data into a csv file that will be
+    correctly parsed"""
+    f = pd.read_csv("data/stock-tx60.csv")
+    keep_col = ['Date', 'Open', 'High', 'Low', 'Close']
+    new_f = f[keep_col]
+    new_f = new_f.apply(lambda x: x.str.replace(',', ''))
+    new_f['Date'] = pd.to_datetime(new_f['Date'], format='%b %d %Y')
+    final_f = new_f
+    final_f.to_csv("data/stock-tx60.csv", index=False)
